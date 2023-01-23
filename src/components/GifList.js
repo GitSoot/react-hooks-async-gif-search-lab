6ef48371f0e68f5762607ef892b8c60dcf7a72e7
Eric Onyango <eric.onyango@student.moringaschool.com>
2023-01-23 20:47:54 +0300

    List container added + styling changes

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -2,13 +2,15 @@ import React from 'react'
 
 const GifList = ({gifs}) => {
   return (
+    <div >
     <ul>
       {gifs.map((gif) => (
-        <li key={gif.url}>
+        <li style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around', border: '2px solid #ff6550', margin: '2px'}} key={gif.url}>
           <img  src={gif.url} alt="searched gif" />
         </li>
       ))}
   </ul>
+  </div>
     
   )
 }
--- src/components/GifSearch.js
+++ src/components/GifSearch.js
@@ -1,26 +1,35 @@
 import React, { useState } from 'react';
 
-const GifSearch = ({ submitSearch }) => {
-    const [searchItem, setSearch] = useState("");
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        submitSearch(searchItem);
-    }
+const GifSearch = ({ submitSearchItem }) => {
+    const [searchItem, setSearchItem] = useState("");
 
     const handleSearch = (e) => {
-        setSearch(e.target.value);
+        setSearchItem(e.target.value);
+    }
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        submitSearchItem(searchItem);
     }
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label >Thinking of a GIF?</label><br></br>
-                <input id="search" placeholder='Type here' type="text" value={searchItem} onChange={handleSearch}/>
-                <button type="submit">Click here!</button>
-            </form>
-        </div>
-    );
+
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Thinking of a GIF?</label>
+        <br></br>
+        <input
+          id="search"
+          placeholder="Type here"
+          type="text"
+          value={searchItem}
+          onChange={handleSearch}
+        />
+        <button type="submit">Click here!</button>
+      </form>
+    </div>
+  );
 };
 
 export default GifSearch;
